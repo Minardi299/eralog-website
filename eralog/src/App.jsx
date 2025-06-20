43b0a04cbf356ec70053c1e297d15c10e227966b
Minardi299 <phu.ngo@outlook.com>
2025-06-19 23:32:06 -0400

    add navbar

--- eralog/src/App.jsx
+++ eralog/src/App.jsx
@@ -1,4 +1,3 @@
-import viteLogo from '/vite.svg'
 import './App.css'
 import { ModeToggle } from '@/components/mode-toggle'
 import { Button } from "@/components/ui/button";
@@ -6,12 +5,26 @@ import { Hero } from "@/components/hero";
 import { ScrollProgress } from "@/components/magicui/scroll-progress";
 import { Bento} from "@/components/bento"
 import { Footer } from '@/components/footer';
+const Navbar = () => {
+  return (
+    <header className="sticky top-0 z-40 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center justify-between mx-auto px-4">
+        <a href="#" className="mr-6 flex items-center space-x-2">
+          <span className="font-bold sm:inline-block">
+            Eralog
+          </span>
+        </a>
+        <ModeToggle />
+      </div>
+    </header>
+  );
+};
 function App() {
 
   return (
     <>
-    <ScrollProgress />
-
+<Navbar />
+      <ScrollProgress /> 
       <Hero />
       
        
